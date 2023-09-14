fae3d29362d48b7ddd2a5b02419dba720a24f92e
renil28 <renil.justin@mca.christuniversity.in>
2023-09-14 12:07:11 +0530

    first commit

---
+++ Day1New/Day1New/Functiondemo.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Day1New
+{
+    internal class Functiondemo
+    {
+        static void Sum(int a, int b)
+        {
+            Console.WriteLine("Sum: " + (a + b));
+        }
+        static void Divide(int a, int b)
+        {
+            Console.WriteLine("Division: " + (a / b));
+
+        }
+        static void Sub(int a, int b)
+        {
+            Console.WriteLine("Sub: " + (a - b));
+
+        }
+        static void Mul(int a, int b)
+        {
+            Console.WriteLine("Multiplication: " + (a * b));
+
+        }
+        //Swapping Numbers
+        public static void Swap(ref int a, ref int b)
+        {
+            int c;
+            c = b;
+            b = a;
+            a = c;
+            Console.WriteLine("a" + a + ", b =" + b);
+        }
+        
+        public static void Out(out int a)
+        {
+            a = 20;
+        }
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.WriteLine("Calculator");
+                Console.WriteLine("1. Addition");
+                Console.WriteLine("2. Subtraction");
+                Console.WriteLine("3. Multiplication");
+                Console.WriteLine("4. Division");
+                Console.WriteLine("5. Swap/Out");
+                Console.WriteLine("Press 0 to exit");
+                Console.WriteLine("Enter your Choice:");
+                int Choice = Convert.ToInt32(Console.ReadLine());
+                if(Choice == 0)
+                {
+                    break;
+                }
+
+
+                Console.WriteLine("Please Enter a Value for Number 1:");
+                int Number1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Please Enter a Value for Number 2:");
+                int Number2 = Convert.ToInt32(Console.ReadLine());
+
+                switch (Choice)
+                {
+                    case 1:
+                        Sum(Number1, Number2);
+                        break;
+                    case 2: 
+                        Sub(Number1,Number2); 
+                        break;
+                    case 3:
+                        Mul(Number1, Number2);
+                        break;
+                    case 4:
+                        Divide(Number1, Number2);
+                        break;
+                    case 5:
+                        Out(out Number1);
+                        Swap(ref Number1,ref  Number2);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Choice");
+                        break;
+
+
+                }
+            } while (true);
+
+
+        }
+
+    }
+}
---
+++ Day1New/Day1New/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace Day1New
+{
+    internal class Program
+    {
+        static void Main1(string[] args)
+        {
+            Console.WriteLine("Please Enter a Value for Number 1:");
+            int Number1 = Convert.ToInt32(Console.ReadLine()); 
+            Console.WriteLine("Please Enter a Value for Number 2:");
+            int Number2 = Convert.ToInt32(Console.ReadLine()); 
+            Console.WriteLine("Please Enter a Value for Number 3");
+            int Number3 = Convert.ToInt32(Console.ReadLine());
+
+            /* Math Operations
+            Console.WriteLine(Number1 + Number2);
+            Console.WriteLine(Number1 * Number2);
+            Console.WriteLine(Number1 / Number2);
+            Console.WriteLine(Number2 - Number1);
+            Console.WriteLine("Modulus: "+( Number1 % Number2));
+            Console.WriteLine("The Entered Value: " + (Number3));
+            */
+
+            //Checking Greater than, less than
+            if (Number1 > Number2)
+            {
+                Console.WriteLine(Number1 + "is greater than " + Number2);
+            }
+            else
+            {
+                Console.WriteLine(Number2 + " is greater than " + Number1);
+            }
+
+            //Check Odd/Even Number
+            if(Number3%2 == 0)
+            {
+                Console.WriteLine(Number3 + " is an even number");
+            }
+            else
+            {
+                Console.WriteLine(Number3 + " is an odd number");
+            }
+
+            //Check greater than with ternary operator
+            string output = (Number1 > Number2) ? (Number1 + " is greater than " + Number2) : (Number2 + " is greater than " + Number1);
+            Console.WriteLine(output);
+           
+            //Check the greatest number from 3 numbers
+            if(Number1>Number2 && Number1 > Number3)
+            {
+                Console.WriteLine(Number1 + " is the greatest number from given 3 numbers");
+            }
+            else if (Number2 > Number1 && Number2 > Number3)
+            {
+                Console.WriteLine(Number2 + " is the greatest number from given 3 numbers");
+            }
+            else
+            {
+                Console.WriteLine(Number3 + " is the greatest number from given 3 numbers");
+            }
+
+
+        }
+    }
+}
