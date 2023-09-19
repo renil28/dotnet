a7e87c11bdfc8abd95e21e609cae5b048f158304
renil28 <renil.justin@mca.christuniversity.in>
2023-09-19 11:38:48 +0530

    Day 3 Files 5th Commit

--- Day3/Day3/AccessSpecifier.cs
+++ Day3/Day3/AccessSpecifier.cs
@@ -6,9 +6,19 @@ using System.Threading.Tasks;
 
 namespace Day3
 {
+    //This program demonstrates the role of access specifiers.
+    //Refer excel sheet to know the differences between them!
+    public class Sample
+    {
+        private protected int a;
+    }
+    public class Derived : Sample
+    {
+        //Console.WriteLine(a);
+    }
     internal class AccessSpecifier
     {
-        public static void Main()
+        public static void Main5()
         {
             Day3.Demo1.Class1 x1 = new Day3.Demo1.Class1();
             Day3.Demo2.Class1 x2 = new Day3.Demo2.Class1();
---
+++ Day3/Day3/ConstVsReadonly.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3
+{ 
+    //Program to demonstrate const and readonly
+    public class DemoReadOnly
+    {
+        public readonly int ab = 1;
+        public DemoReadOnly(int a)
+        {
+            ab = a;
+        }
+        public void Print()
+        {
+            Console.WriteLine("Value is: " + ab);
+        }
+    }
+    internal class ConstVsReadonly
+    {
+        public static void Main(string[] args)
+        {
+            const int a = 1;
+            //a = 2
+            DemoReadOnly d = new DemoReadOnly(2);
+            //d.ab = 1;
+            d.Print();
+        }
+    }
+}
