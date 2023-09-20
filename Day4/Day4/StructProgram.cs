392fb684aa733ba12650b720d5a1ed2e587f470b
renil28 <renil.justin@mca.christuniversity.in>
2023-09-20 09:48:43 +0530

    Day 4 Files 1st Commit

---
+++ Day4/Day4/StructProgram.cs
@@ -0,0 +1,47 @@
+﻿namespace Day4
+{
+    //Example of using struct in the program
+    struct Employee
+    {
+        public int EmployeeId;
+        String EmployeeName;
+
+        public void GetEmployeeDetails()
+        {
+            Console.WriteLine("Please Enter Employee ID");
+            EmployeeId = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please Enter Employee Name");
+            EmployeeName = Console.ReadLine();
+        }
+
+        public void PrintEmployeeDetails()
+        {
+            Console.WriteLine("Employee ID: " + EmployeeId);
+            Console.WriteLine("Employee Name: " + EmployeeName);
+        }
+    };
+
+    /* Destructor Example
+    class Sample { 
+
+        public int MyProperty { get; set; }
+        public Sample()
+        {
+            
+        }
+
+        //This is a destructor
+        ~Sample() { }
+       //Destructor releases the memory
+    }
+    */
+    internal class StructProgram
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee();
+            emp.GetEmployeeDetails();
+            emp.PrintEmployeeDetails();
+        }
+    }
+}
