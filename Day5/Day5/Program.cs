49c02a0200a020c43b182fb9e12c120d169fa6de
renil28 <renil.justin@mca.christuniversity.in>
2023-09-21 09:53:39 +0530

    Day 5 Files 2nd Commit

--- Day5/Day5/Program.cs
+++ Day5/Day5/Program.cs
@@ -2,12 +2,22 @@
 {
     internal class Program
     {
+        //This Program Demonstrates File Handling with the use of 
+        //StreamReader and StreamWriter Class
         static void Main(string[] args)
         {
-            StreamWriter s = new StreamWriter("C:\\Users\\Administrator\\Desktop\\Dot Net Lab\\Day5\\Day5\\files\\test.txt");
-            s.Write("Hello Renil");
+            string FilePath = "C:\\Users\\Administrator\\Desktop\\Dot Net Lab\\Day5\\Day5\\files\\test.txt";
+            StreamWriter s = new StreamWriter(FilePath);
+            Console.WriteLine("Please Enter Some Value");
+            string valueToWriteInFile = Console.ReadLine();
+            s.WriteLine(valueToWriteInFile);
             s.Flush();
             s.Close();
+
+            StreamReader sr = new StreamReader(FilePath);
+            sr.BaseStream.Seek(0, SeekOrigin.Begin);
+            string output = sr.ReadLine();
+            Console.WriteLine("Value from file is " + output);
             Console.WriteLine("Hello, World!");
         }
     }
