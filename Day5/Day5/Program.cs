90c7b7dca3c6f283b2f7cde37728bd16a006dffb
renil28 <renil.justin@mca.christuniversity.in>
2023-09-21 09:37:33 +0530

    Day 5 Files 1st Commit

---
+++ Day5/Day5/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Day5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter s = new StreamWriter("C:\\Users\\Administrator\\Desktop\\Dot Net Lab\\Day5\\Day5\\files\\test.txt");
+            s.Write("Hello Renil");
+            s.Flush();
+            s.Close();
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
