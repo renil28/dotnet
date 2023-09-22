6f4a87de79888a596aef40dcfeb260554fc688f9
renil28 <renil.justin@mca.christuniversity.in>
2023-09-22 09:36:59 +0530

    Day 6 Files 1st Commit

---
+++ Day6/Day6/Lambda.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6
+{
+    //This program demonstrates the use of Lambda and Linq 
+    internal class Lambda
+    {
+        public static void Main()
+        {
+            //Using lambda to find odd/even numbers from a list
+            List<int> Numbers = new List<int>() { 1,2,3,4,5,6};
+            List<int> evenNumbers = Numbers.FindAll(x => (x%2) == 0);
+            Console.WriteLine("Even Numbers: ");
+            foreach (int number in evenNumbers)
+            {
+                Console.WriteLine(number);
+            }
+            List<int> oddNumbers = Numbers.FindAll(x => (x % 2) != 0);
+            Console.WriteLine("Odd Numbers: ");
+            foreach (int number in oddNumbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
---
+++ Day6/Day6/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Day6
+{
+    //This program demonstrates the use of extension methods
+    class Test
+    {
+        public void Print()
+        {
+            Console.WriteLine("Print Method");
+        }
+    }
+
+    static class TestExtension
+    {
+        static public void PrintNew(this Test test)
+        {
+            Console.WriteLine("Print New Definition");
+        }
+    }
+    internal class Program
+    {
+     
+        static void Main1(string[] args)
+        {
+            Test test = new Test();
+            test.Print();
+            test.PrintNew();
+            //Test test1 = new Test();
+            //test1.PrintNew();
+        }
+    }
+}
