16186c6731d16a00f83fef376d3d33587bb2902d
renil28 <renil.justin@mca.christuniversity.in>
2023-09-22 09:57:41 +0530

    Day 6 Files 2nd Commit

---
+++ Day6/Day6/EventsDemo.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6
+{
+    //Using Events in C#
+    delegate void OrderEventHandler();
+    class Order
+    {
+        public event OrderEventHandler OnOrderCreated;
+        public void Create()
+        {
+            Console.WriteLine("Order Created");
+            //SMS.send();
+            //Email.send();
+            if(OnOrderCreated != null)
+            {
+                OnOrderCreated();
+            }
+        }
+    }
+
+    class SMS
+    {
+        public static void send()
+        {
+            Console.WriteLine("SMS sent");
+        }
+    }
+
+    class Email
+    {
+        public static void send()
+        {
+            Console.WriteLine("Email sent");
+        }
+    }
+    internal class EventsDemo
+    {
+        public static void Main() 
+        {
+            var Order = new Order();
+            Order.OnOrderCreated += Email.send;
+            Order.OnOrderCreated += SMS.send;
+            Order.Create();
+
+        }
+    }
+}
--- Day6/Day6/Lambda.cs
+++ Day6/Day6/Lambda.cs
@@ -6,10 +6,23 @@ using System.Threading.Tasks;
 
 namespace Day6
 {
+    //Implementation of Partial Class
+    partial class Part1
+    {
+        public void Print1()
+        {
+            Console.WriteLine("Hi, I am from Part1");
+        }
+    }
+    partial class Part1 { 
+        public void Print2() { 
+            Console.WriteLine("Hi I amd from Part2");
+        }
+    }
     //This program demonstrates the use of Lambda and Linq 
     internal class Lambda
     {
-        public static void Main()
+        public static void Main2()
         {
             //Using lambda to find odd/even numbers from a list
             List<int> Numbers = new List<int>() { 1,2,3,4,5,6};
