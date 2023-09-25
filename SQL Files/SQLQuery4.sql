9dfbe8391bd3670df638ddf261031d5feff9f812
renil28 <renil.justin@mca.christuniversity.in>
2023-09-25 09:53:43 +0530

    Day 7 Files Commit with sqlquery file 4

---
+++ SQL Files/SQLQuery4.sql
@@ -0,0 +1,11 @@
+use MyDB;
+
+select * from Employee;
+create table student(id int identity(1,1), FirstName varchar(200) NOT NULL, 
+LastName varchar(200), ClassName int, CreatedOn datetime DEFAULT GETDATE(), PRIMARY KEY(id) )
+
+select GETDATE();
+
+select * from student;
+
+insert into student(FirstName, LastName, ClassName) VALUES('Renil', 'Justin', 1)
