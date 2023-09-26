6edf07b51f03fd2ab88b7d04b4f238be1751f421
renil28 <renil.justin@mca.christuniversity.in>
2023-09-26 12:27:04 +0530

    Day 8 DBConnection Commit 3

---
+++ DBconnection/CodeFirst/CourseDBContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirst
+{
+    public class CourseDBContext : DbContext
+    {
+        public CourseDBContext()
+        {
+        }
+
+        public CourseDBContext(DbContextOptions<CourseDBContext> options)
+            : base(options)
+        {
+        }
+        public virtual DbSet<Course> Course { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+     => optionsBuilder.UseSqlServer("Data Source=EC2AMAZ-EHR6SVV;Initial Catalog=CourseDB;Integrated Security=True;TrustServerCertificate=True");
+
+    }
+
+}
---
+++ DBconnection/DBconnection/Migrations/20230926062445_CourseM.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DBconnection.Migrations
+{
+    /// <inheritdoc />
+    public partial class CourseM : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    bookname = table.Column<string>(type: "varchar(100)", unicode: false, maxLength: 100, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__Books__3213E83F002218CE", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+        }
+    }
+}
