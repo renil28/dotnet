4d802be456fc7cfb062cba67512b5e784b200cbc
renil28 <renil.justin@mca.christuniversity.in>
2023-09-27 10:09:06 +0530

    WebApi Commit 2

---
+++ DemoWebApp/DemoWebApp/Controllers/ValuesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoWebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public string Index()
+        {
+            return "Renil";
+        }
+    }
+}
